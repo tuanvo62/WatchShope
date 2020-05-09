d09add94351b6813850bd05a3aafc2e0982535f7
tuan <debronees2@gmail.com>
2020-05-09 21:35:13 +0700

    first commit

---
+++ img/javascript.js
@@ -0,0 +1,56 @@
+﻿$(function (){
+    $("#form1").validate({
+        rules:
+        {
+            hoten:
+            {
+                required: true,
+                minlength: 3
+                },
+            password:
+            {
+                required: true,
+                minlength: 3,
+                maxlength: 10
+                },
+            comfirmPassword:
+            {
+                required: true,
+                minlength: 3,
+                equalTo: "#password",
+                },
+            email:
+            {
+                required: true,
+                email: true,
+                accept: "gmail.com|icloud.com"
+            },
+            },
+        messages:
+        {
+            hoten:
+            {
+                required: "User name not empty",
+                minlength: "User name less than 3 character."
+                },
+            password:
+            {
+                required: "Password not empty",
+                minlength: "Password less than 3 character",
+                maxlength: "Password more than 10 character"
+                },
+            comfirmPassword:
+            {
+                required: "ComfirmPassword not empty",
+                equalTo: "Password no match"
+                },
+            email:
+            {
+                required: "Email not empty",
+                accept: "Accept gmail or icloud only"
+            },
+            },
+        errorLableContainer: "#errorList",
+        wrapper: "li"
+    })
+    });
---
+++ js/javascript.js
@@ -0,0 +1,68 @@
+﻿// When the user scrolls down 1050px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 1050 ) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
+
+//check form
+$(function (){
+    $("#form1").validate({
+        rules:
+        {
+            hoten:
+            {
+                required: true,
+                minlength: 3
+                },
+            password:
+            {
+                required: true,
+                minlength: 3,
+                maxlength: 10
+                },
+            comfirmPassword:
+            {
+                required: true,
+                minlength: 3,
+                equalTo: "#password",
+                },
+            email:
+            {
+                required: true,
+                email: true,
+                accept: "gmail.com|icloud.com"
+            },
+            },
+        messages:
+        {
+            hoten:
+            {
+                required: "Tên đăng nhập không được để trống",
+                minlength: "Tên đăng nhặp phải lớn hơn 3 ký tự"
+                },
+            password:
+            {
+                required: "Mật khẩu không được để trống",
+                minlength: "Mật khẩu phải hơn 3 ký tự",
+                maxlength: "Mật khẩu phải ít hơn 10 ký tự"
+                },
+            comfirmPassword:
+            {
+                required: "Xác nhận lại mật khẩu",
+                equalTo: "Mật khẩu khôgn đúng"
+                },
+            email:
+            {
+                required: "Email không được để trống",
+                accept: "Chỉ chấp nhận gmail hoặc icloud"
+            },
+            },
+        errorLableContainer: "#errorList",
+        wrapper: "li"
+    })
+});
+////
